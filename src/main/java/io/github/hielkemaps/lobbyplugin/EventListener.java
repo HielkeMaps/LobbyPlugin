0550701a042a11d956dab1bc30f7d68f12cf46ff
Hielke <hielkemaps@gmail.com>
2025-01-25 13:48:58 +0100

    Add launchpad, disable join/leave message

--- src/main/java/io/github/hielkemaps/lobbyplugin/EventListener.java
+++ src/main/java/io/github/hielkemaps/lobbyplugin/EventListener.java
@@ -3,22 +3,124 @@ package io.github.hielkemaps.lobbyplugin;
 import io.github.hielkemaps.lobbyplugin.wrapper.PlayerManager;
 import io.github.hielkemaps.lobbyplugin.wrapper.PlayerWrapper;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerToggleFlightEvent;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.player.*;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
 
 public class EventListener implements Listener {
+    private final Plugin plugin;
+    private Material launchPadPlate;
+    private Material launchPadBase;
+    private double launchForward;
+    private double launchUpward;
+
+    public EventListener(Plugin plugin) {
+        this.plugin = plugin;
+        loadConfig();
+    }
+
+    public void loadConfig() {
+        launchPadPlate = Material.valueOf(plugin.getConfig().getString("launchpad.plate"));
+        launchPadBase = Material.valueOf(plugin.getConfig().getString("launchpad.base"));
+        launchForward = plugin.getConfig().getDouble("launchpad.strength.forward");
+        launchUpward = plugin.getConfig().getDouble("launchpad.strength.upward");
+    }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent e){
-        e.getPlayer().setAllowFlight(true);
+    public void onPressurePlate(PlayerInteractEvent e) {
+        if (e.getAction() != Action.PHYSICAL) return;
+
+        Block block = e.getClickedBlock();
+        if (block == null || block.getType() != launchPadPlate) return;
+
+        Block baseBlock = block.getRelative(BlockFace.DOWN);
+        if (baseBlock.getType() != launchPadBase) return;
+
+        Player player = e.getPlayer();
+        Vector direction = player.getLocation().getDirection().multiply(launchForward).setY(launchUpward);
+        player.setVelocity(direction);
+
+        player.playSound(player.getLocation(), Sound.ENTITY_BREEZE_SHOOT, 1.0f, 1.0f);
+
+        int[] ticks = {0};
+        Bukkit.getScheduler().runTaskTimer(plugin, (task) -> {
+            if (ticks[0]++ >= 4) {
+                task.cancel();
+                return;
+            }
+            Location particleLoc = player.getLocation().add(
+                    Math.random() * 0.6 - 0.3,
+                    0.5 + Math.random() * 0.4 - 0.2,
+                    Math.random() * 0.6 - 0.3
+            );
+            player.getWorld().spawnParticle(Particle.GUST, particleLoc, 1, 0.3, 0.1, 0.3, 0.01);  }, 2L, 2L);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        Player player = e.getPlayer();
+
+        // Teleport to spawn and set flight
+        player.teleport(player.getWorld().getSpawnLocation());
+        player.setAllowFlight(true);
+        player.setGameMode(GameMode.ADVENTURE);
+
+        Component border = LegacyComponentSerializer.legacyAmpersand()
+                .deserialize("&8&m+---------------***---------------+");
+
+        Component website = LegacyComponentSerializer.legacyAmpersand()
+                .deserialize("&r  &2&lWEBSITE &fhielkemaps.com")
+                .clickEvent(ClickEvent.openUrl("https://hielkemaps.com"))
+                .hoverEvent(HoverEvent.showText(Component.text("Click to visit!")));
+
+        Component discord = LegacyComponentSerializer.legacyAmpersand()
+                .deserialize("&r  &9&lDISCORD &ddiscord.gg/6RzKxbH")
+                .clickEvent(ClickEvent.openUrl("https://discord.gg/6RzKxbH"))
+                .hoverEvent(HoverEvent.showText(Component.text("Click to join!")));
+
+        Component welcome = LegacyComponentSerializer.legacyAmpersand()
+                .deserialize("&r    &7Welcome, &b&n" + player.getName() + "&r &7to the Server!");
+
+        Component empty = Component.empty();
+
+        player.sendMessage(border);
+        player.sendMessage(empty);
+        player.sendMessage(welcome);
+        player.sendMessage(empty);
+        player.sendMessage(website);
+        player.sendMessage(discord);
+        player.sendMessage(empty);
+        player.sendMessage(border);
+
+        // Disable default join message
+        e.joinMessage(null);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e) {
+        e.quitMessage(null);
+    }
+
+    @EventHandler
+    public void onFoodLevelChange(FoodLevelChangeEvent e) {
+        e.setCancelled(true);
+        if (e.getEntity() instanceof Player) {
+            e.getEntity().setFoodLevel(20);
+        }
     }
 
     @EventHandler
@@ -39,16 +141,13 @@ public class EventListener implements Listener {
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-
-        if(player.isOnGround()){
-            if(!player.getAllowFlight()) {
-                player.setAllowFlight(true);
-            }
+        if(player.isOnGround() && !player.getAllowFlight()) {
+            player.setAllowFlight(true);
         }
     }
 
     @EventHandler
-    public void OnPlayerInteract(PlayerInteractEvent e){
+    public void OnPlayerInteract(PlayerInteractEvent e) {
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock().getType() == Material.CHEST) {
             Chest chest = (Chest) e.getClickedBlock().getState();
             if(chest.getLock().equals("shop")){
--- src/main/java/io/github/hielkemaps/lobbyplugin/Main.java
+++ src/main/java/io/github/hielkemaps/lobbyplugin/Main.java
@@ -1,16 +1,32 @@
 package io.github.hielkemaps.lobbyplugin;
 
+import dev.jorel.commandapi.CommandAPI;
+import dev.jorel.commandapi.CommandAPICommand;
+import dev.jorel.commandapi.CommandPermission;
 import io.github.hielkemaps.lobbyplugin.commands.Fly;
-import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin {
+    private EventListener eventListener;
 
     @Override
     public void onEnable() {
-        Bukkit.getPluginManager().registerEvents(new EventListener(), this);
+        saveDefaultConfig();
 
+        eventListener = new EventListener(this);
+        getServer().getPluginManager().registerEvents(eventListener, this);
         new Fly();
+
+        new CommandAPICommand("lobbyplugin")
+                .withSubcommand(new CommandAPICommand("reload")
+                        .withPermission(CommandPermission.fromString("lobbyplugin.command.reload"))
+                        .executes((sender, args) -> {
+                            reloadConfig();
+                            eventListener.loadConfig();
+                            sender.sendMessage(Component.text("Config reloaded!").color(NamedTextColor.GREEN));
+                        }))
+                .register();
     }
 }
